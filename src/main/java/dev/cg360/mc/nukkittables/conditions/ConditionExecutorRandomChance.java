44a4c206466993bc6d957c101c22af2197ab7666
Alemiz <alex@mizerak.eu>
2023-06-22 23:02:40 +0200

    Improve chance condition

--- src/main/java/dev/cg360/mc/nukkittables/conditions/ConditionExecutorRandomChance.java
+++ src/main/java/dev/cg360/mc/nukkittables/conditions/ConditionExecutorRandomChance.java
@@ -2,22 +2,20 @@ package dev.cg360.mc.nukkittables.conditions;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
 import dev.cg360.mc.nukkittables.context.RollContext;
 import dev.cg360.mc.nukkittables.executors.TableConditionExecutor;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ConditionExecutorRandomChance extends TableConditionExecutor<RollContext> {
 
     @Override
     public boolean isConditionPassed0(RollContext context, JsonObject data) {
         JsonElement element = data.get("chance");
-        if(element instanceof JsonPrimitive){
-            JsonPrimitive primitive = (JsonPrimitive) element;
-            if(primitive.isBoolean()){
-                return primitive.getAsBoolean();
-            }
+        if (!element.isJsonPrimitive() || !element.getAsJsonPrimitive().isNumber()) {
+            return false;
         }
-        return true;
+        return element.getAsDouble() < ThreadLocalRandom.current().nextDouble();
     }
 
     @Override
